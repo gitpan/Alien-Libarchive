cabf3128b2da196713096ed5a308d74051189cc2
Graham Ollis <plicease@cpan.org>
2014-01-29 16:32:08 +0000

    Import of PLICEASE/Alien-Libarchive-0.15 from CPAN.
    gitpan-cpan-distribution: Alien-Libarchive
    gitpan-cpan-version:      0.15
    gitpan-cpan-path:         PLICEASE/Alien-Libarchive-0.15.tar.gz
    gitpan-cpan-author:       PLICEASE
    gitpan-cpan-maturity:     released

--- lib/Alien/Libarchive.pm
+++ lib/Alien/Libarchive.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 # ABSTRACT: Build and make available libarchive
-our $VERSION = '0.14'; # VERSION
+our $VERSION = '0.15'; # VERSION
 
 
 if($^O eq 'MSWin32')
@@ -36,7 +36,7 @@ Alien::Libarchive - Build and make available libarchive
 
 =head1 VERSION
 
-version 0.14
+version 0.15
 
 =head1 SYNOPSIS
 
