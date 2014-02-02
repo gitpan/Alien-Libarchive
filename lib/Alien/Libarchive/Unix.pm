d131b45e61baa29868e8bb28192c02ba8c37d0e2
Graham Ollis <plicease@cpan.org>
2014-02-02 09:31:18 +0000

    Import of PLICEASE/Alien-Libarchive-0.16 from CPAN.
    gitpan-cpan-distribution: Alien-Libarchive
    gitpan-cpan-version:      0.16
    gitpan-cpan-path:         PLICEASE/Alien-Libarchive-0.16.tar.gz
    gitpan-cpan-author:       PLICEASE
    gitpan-cpan-maturity:     released

--- lib/Alien/Libarchive/Unix.pm
+++ lib/Alien/Libarchive/Unix.pm
@@ -11,7 +11,7 @@ use File::ShareDir ();
 use File::Spec;
 
 # ABSTRACT: Build and make available libarchive (machinery for Unix)
-our $VERSION = '0.15'; # VERSION
+our $VERSION = '0.16'; # VERSION
 
 
 # workaround for Alien::Base gh#30
@@ -105,7 +105,7 @@ Alien::Libarchive::Unix - Build and make available libarchive (machinery for Uni
 
 =head1 VERSION
 
-version 0.15
+version 0.16
 
 =head1 SEE ALSO
 
